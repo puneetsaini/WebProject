4a7d2ac05732c75e9fb500362666d72269653354
puneetsaini <sainijasdeep@yahoo.com>
2014-05-20 09:26:37 -0400

    Login files

--- everydayjobs/application/controllers/login.php
+++ everydayjobs/application/controllers/login.php
@@ -1,19 +1,39 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-    public function index()
-    {
-        echo 'Login pge';
-        $this->home();
+    function __construct() {
+        parent::__construct();
+         $this->load->helper('url');  
+        
     }
-    public function home(){
-        $this->load->view("view_home");
+    public function index(){
+        // Load our view to be displayed
+        // to the user
+        $msg='';
+        $data['msg']=$msg;
+        
+        $this->load->view('view_login', $data);
     }
-
-    public function join()
-    {
-        $this->load->view("view_join");
+    public function process(){
+        // Load the model
+        $this->load->model('model_login');
+        // Validate the user can login
+        $result = $this->model_login->validate();
+        // Now we verify the result
+        if(! $result){
+            // If user did not validate, then show them login page again
+            $msg = '<font color=red>Invalid usrname or password</font><br/>';
+            $this->index();
+        }else{
+            // If user did validate, 
+            // Send them to members area
+            redirect('member_home');
+             
+        }        
     }
-           
+     public function do_logout(){
+        $this->session->sess_destroy();
+        redirect('login');
+    }
     
 }
